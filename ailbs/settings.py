7ab3e4c14bd86ce5dc6bd888e276b4e4429aca91
reydon <dhruvarora@delhinerds.com>
2018-12-09 17:09:11 +0530

    added cors headwers

--- ailbs/settings.py
+++ ailbs/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'registration',
     'widget_tweaks',
     'import_export',
+    'corsheaders',
 ]
 
 SITE_ID = 1
@@ -54,12 +55,15 @@ SITE_ID = 1
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'ailbs.urls'
 
