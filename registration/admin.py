de184c7ddfd7d534c259dfb5ccc09fa5ee17063e
reydon227 <dhruvarora@delhinerds.com>
2019-03-13 15:28:29 +0530

    orgations fixes

--- registration/admin.py
+++ registration/admin.py
@@ -6,6 +6,9 @@ from import_export.admin import ImportExportActionModelAdmin
 class RegistrationAdmin(ImportExportActionModelAdmin):
     list_display = ["first_name", "middle_name", "last_name", "email", "hospital", "speciality"]
 
+class CommonAdmin(ImportExportActionModelAdmin):
+    pass
+
 admin.site.register(Registration, RegistrationAdmin)
 admin.site.register(AnesthesiaRegistration, RegistrationAdmin)
-admin.site.register(AbstractSubmission)
+admin.site.register(AbstractSubmission, CommonAdmin)
